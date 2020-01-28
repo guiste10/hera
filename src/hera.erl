77edc5422d1977cbdbea29a414d4075ea3e4f03d
Guillaume Neirinckx <guiste10@gmail.com>
2020-01-28 17:24:49 +0100

    test mtn worker pos

--- src/hera.erl
+++ src/hera.erl
@@ -38,9 +38,9 @@ stop(_State) -> ok.
 %% -------------------------------------------------------------------
 launch_app() ->
   hera_pool:start_pool(pool1, 1, {hera_measure, start_link, []}),
-  hera_pool:run(pool1, [1000]).
-  %hera_pool:start_pool(pool2, 1, {hera_position, start_link, []}),
-  %hera_pool:run(pool2, [1000]).
+  hera_pool:run(pool1, [1000]),
+  hera_pool:start_pool(pool2, 1, {hera_position, start_link, []}),
+  hera_pool:run(pool2, [2000]).
 
 
 
