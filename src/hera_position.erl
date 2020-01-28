5e98d48b9c92eb2ddfe03e7b48b67b28c4f88f84
Guillaume Neirinckx <guiste10@gmail.com>
2020-01-28 18:28:08 +0100

    enlever le readcardinality

--- src/hera_position.erl
+++ src/hera_position.erl
@@ -18,12 +18,12 @@ stop(Pid) ->
 
 init(Delay) ->
     Id = {<<"measurements">>,state_orset},
-    io:format("position: wait ~n"),
-    lasp:read(Id, {cardinality, 2}), % wait until set contains 2 measures
+    %io:format("position: wait ~n"),
+    %lasp:read(Id, {cardinality, 2}), % wait until set contains 2 measures
     Separation = 400,
     Iter = 0,
     io:format("position: init ~n"),
-    {ok, {Delay, Id, Separation, Iter}, Delay}. % {ok, state, timeout}
+    {ok, {Delay, Id, Separation, false, Iter}, Delay}. % {ok, state, timeout}
 
 handle_call(stop, _From, State) ->
     {stop, normal, ok, State};
@@ -33,18 +33,35 @@ handle_call(_Msg, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
         
-handle_info(timeout, {Delay, Id, Separation, Iter}) ->
-    {ok, Value} = lasp:query(Id), 
-    [{R1, _},{R2, _}] = sets:to_list(Value), % [{measure, name}, ...]
-    R1Sq = math : pow ( R1 , 2) ,
-    R2Sq = math : pow ( R2 , 2) ,
-    S2 = 2 * Separation ,
-    SSq = math : pow ( Separation , 2) ,
-    X = ( R1Sq - R2Sq + SSq ) / S2 ,
-    Y1 = math : sqrt ( R1Sq - math : pow (X , 2) ) ,
-    Y2 = - Y1 ,
-    io:format("position: (~p, ~p) or (~p, ~p) ~n", [X, Y1, X, Y2]),
-    {noreply, {Delay, Id, Separation, Iter+1}, Delay}.
+handle_info(timeout, {Delay, Id, Separation, Ready, Iter}) ->
+    {ok, Values} = lasp:query(Id), 
+    if 
+        Ready =:= false ->
+            Length = sets:size(Values),
+            if 
+                Length =:= 2 ->
+                    Ready2 = false;
+                true ->
+                    Ready2 = true
+            end;
+        true ->
+            Ready2 = true
+    end,
+    if
+        Ready2 =:= true ->
+            [{R1, _},{R2, _}] = sets:to_list(Values), % [{measure, name}, ...]
+            R1Sq = math : pow ( R1 , 2) ,
+            R2Sq = math : pow ( R2 , 2) ,
+            S2 = 2 * Separation ,
+            SSq = math : pow ( Separation , 2) ,
+            X = ( R1Sq - R2Sq + SSq ) / S2 ,
+            Y1 = math : sqrt ( R1Sq - math : pow (X , 2) ) ,
+            Y2 = - Y1 ,
+            io:format("position: (~p, ~p) or (~p, ~p) ~n", [X, Y1, X, Y2]);
+        true ->
+            io:format("position: not definable ~n")
+    end,
+    {noreply, {Delay, Id, Separation, Ready2, Iter+1}, Delay}.
 %% We cannot use handle_info below: if that ever happens,
 %% we cancel the timeouts (Delay) and basically zombify
 %% the entire process. It's better to crash in this case.
