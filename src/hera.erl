19b298f44b4b1e955193bf07665879b8ed216f59
Guillaume Neirinckx <guiste10@gmail.com>
2020-02-26 13:11:03 +0100

    fix

--- src/hera.erl
+++ src/hera.erl
@@ -8,7 +8,7 @@
 -include("hera.hrl").
 
 %% API/home/julien/home/julien/home/julien
--export([launch_app/1]).
+-export([launch_app/2]).
 -export([clusterize/0]).
 -export([fake_sonar_get/0]).
 -export([send/1]).
@@ -51,9 +51,9 @@ stop(_State) -> ok.
 %%        -> ok
 %% @end
 %% -------------------------------------------------------------------
-launch_app(Delay) ->
+launch_app(Delay, Max_iter) ->
   hera_pool:start_pool(pool1, 1, {single_sonar_test, start_link, []}),
-  hera_pool:run(pool1, [Delay]).
+  hera_pool:run(pool1, [Delay, Max_iter]).
 
 %% -------------------------------------------------------------------
 %% @doc
