7157d24b3cfdc46b21d3e6fbd07c6b5e12ac7b71
Guillaume Neirinckx <guiste10@gmail.com>
2020-02-26 14:35:28 +0100

    encore test

--- src/single_sonar_test.erl
+++ src/single_sonar_test.erl
@@ -39,7 +39,7 @@ handle_info(timeout, {Iter, Max_iter, Delay, File, File_name}) ->
     %Measure = hera:fake_sonar_get(),
     %Measure_str = integer_to_list(Measure), % pour faux sonar (integer)
 
-    io:format("measure: (~s) ~n", [Measure_str]), % print
+    %io:format("measure: (~s) ~n", [Measure_str]), % print
     Row = Measure_str ++ "\n",
     file:pwrite(File, eof, [Row]),
     if
