aaa434423c080c3be0c53b4b22b705a6c708c100
Julien Bastin <julien.bastin@student.uclouvain.be>
2020-02-29 22:59:00 +0100

    add parenthesis

--- src/single_sonar_test.erl
+++ src/single_sonar_test.erl
@@ -149,7 +149,7 @@ terminate(_Reason, _State) -> ok.
 
 -spec make_measures(State :: state()) -> ok.
 make_measures(State) ->
-    Measure = State#state.func,
+    Measure = State#state.func(),
     Measure_str = io_lib:format("~.2f", [Measure]), % pour vrai sonar (float)
     %Measure = hera:fake_sonar_get(),
     %Measure_str = integer_to_list(Measure), % pour faux sonar (integer)
