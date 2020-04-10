8b8ccfe979ac53616dd76ab6b335eba3f300740c
Guillaume Neirinckx <guiste10@gmail.com>
2020-04-10 11:46:11 +0200

    création module filter

---
+++ src/hera_filter.erl
@@ -0,0 +1,99 @@
+-module(hera_filter).
+-behaviour(gen_server).
+-export([start_link/0, stop/1]).
+-export([init/1, handle_call/3, handle_cast/2,
+handle_info/2, code_change/3, terminate/2]).
+
+%%====================================================================
+%% Macros
+%%====================================================================
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% Records
+%%====================================================================
+
+-record(state, {
+    previous_dist :: function(),
+    num_measures :: integer(),
+    num_filtered :: integer()
+}).
+-type state() :: #state{}.
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @doc Spawns the server and registers the local name (unique)
+-spec(start_link() ->
+    {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop(Pid) ->
+    gen_server:call(Pid, stop).
+
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%% @private
+%% @doc Initializes the server
+-spec(init([]) ->
+    {ok, State :: state()} | {ok, State :: state(), timeout() | hibernate} |
+    {stop, Reason :: term()} | ignore).
+init([]) ->
+    {ok, #state{previous_dist = -1, num_measures = 0, num_filtered = 0}}.
+
+%% @private
+%% @doc Handling call messages
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+    State :: state()) ->
+    {reply, Reply :: term(), NewState :: state()} |
+    {reply, Reply :: term(), NewState :: state(), timeout() | hibernate} |
+    {noreply, NewState :: state()} |
+    {noreply, NewState :: state(), timeout() | hibernate} |
+    {stop, Reason :: term(), Reply :: term(), NewState :: state()} |
+    {stop, Reason :: term(), NewState :: state()}).
+handle_call(stop, _From, State) ->
+    {stop, normal, ok, State};
+handle_call(_Msg, _From, State) ->
+    {noreply, State}.
+
+%% @private
+%% @doc Handling cast messages
+-spec(handle_cast(Request :: term(), State :: state()) ->
+    {noreply, NewState :: state()} |
+    {noreply, NewState :: state(), timeout() | hibernate} |
+    {stop, Reason :: term(), NewState :: state()}).
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%% @private
+%% @doc Handling all non call/cast messages
+-spec(handle_info(Info :: timeout() | term(), State :: state()) ->
+    {noreply, NewState :: state()} |
+    {noreply, NewState :: state(), timeout() | hibernate} |
+    {stop, Reason :: term(), NewState :: state()}).
+handle_info(_Msg, State) ->
+    {noreply, State}.
+
+
+%% @private
+%% @doc Convert process state when code is changed
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: state(),
+    Extra :: term()) ->
+    {ok, NewState :: state()} | {error, Reason :: term()}).
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% @private
+%% @doc This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: state()) -> term()).
+terminate(_Reason, _State) -> ok.
