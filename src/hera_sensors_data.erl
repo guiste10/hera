12b93a600307ebf73598287aa4f439a8371d052d
Guillaume Neirinckx <guiste10@gmail.com>
2020-05-22 11:06:46 +0200

    do measures only + log timestamp of measure as well

--- src/hera_sensors_data.erl
+++ src/hera_sensors_data.erl
@@ -179,7 +179,7 @@ handle_cast({store_data, {Name, {Node, Seqnum, Measure}}}, State = #state{data =
   {noreply, State#state{data = New_data#{Name => Dict2}}};
 handle_cast({log_measure, {Name, {Node, Seqnum, {Data, TimeStamp}}}}, State = #state{measures_logger_configs = Log_Conf}) ->
   check_handlers(Name, Node, Log_Conf, measures),
-  logger:debug("~p, ~p at ~p", [Seqnum, Data, TimeStamp], #{domain => [measures, Node, Name]}), %% Log data to file
+  logger:debug("~p, ~p, ~p", [Seqnum, Data, TimeStamp], #{domain => [measures, Node, Name]}), %% Log data to file
   {noreply, State};
 handle_cast({log_calculation, {Name, {Node, Seqnum, Data}}}, State = #state{calculations_logger_configs = Log_Conf}) ->
   check_handlers(Name, Node, Log_Conf, calculations),
