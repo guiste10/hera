8a83590716b3a3a5f6535abff7f78e38961a5487
Julien Bastin <julien.bastin@student.uclouvain.be>
2020-07-01 23:10:55 +0200

    move recursion

--- src/hera.erl
+++ src/hera.erl
@@ -89,8 +89,8 @@ launch_app(Measurements, Calculations, Master) ->
       hera_pool:start_pool(hera_global_pool, 1, {hera_global_sync, start_link, []}),
       hera_pool:run(hera_global_pool, []),
       SyncMeas = lists:filter(fun({_Name, Meas}) -> maps:get(synchronization, Meas) end, Measurements),
-      hera_pool:start_pool(global_dispatch_pool, length(SyncMeas), {hera_global_dispatch, start_link, []});
-      %%[hera_pool:run(global_dispatch_pool, [Name, hera_utils:concat_atoms(dispatch_, Name)]) || {Name, _M} <- SyncMeas];
+      hera_pool:start_pool(global_dispatch_pool, length(SyncMeas), {hera_global_dispatch, start_link, []}),
+      [hera_pool:run(global_dispatch_pool, [Name, hera_utils:concat_atoms(dispatch_, Name)]) || {Name, _M} <- SyncMeas];
     false -> not_master
   end,
 
